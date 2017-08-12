fdf45dc1d36d353a19115806466b31bfdb302a19
Niklas Barsk <nibarius@gmail.com>
2017-08-12 20:42:14 +0200

    Improve the add/edit car dialog Nicer layout and don't allow saving with empty text fields.

--- app/src/main/java/se/barsk/park/EditCarDialog.kt
+++ app/src/main/java/se/barsk/park/EditCarDialog.kt
@@ -5,6 +5,9 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.View
 import android.view.WindowManager
 import android.widget.EditText
@@ -35,6 +38,14 @@ open class EditCarDialog(val ownCar: OwnCar? = null) : DialogFragment() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        updatePositiveButton()
+        val dialogWatcher = DialogTextWatcher()
+        regNoView.addTextChangedListener(dialogWatcher)
+        ownerView.addTextChangedListener(dialogWatcher)
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity)
         if (ownCar != null) {
@@ -45,11 +56,23 @@ open class EditCarDialog(val ownCar: OwnCar? = null) : DialogFragment() {
                 .setTitle(dialogTitle)
                 .setNegativeButton(R.string.cancel, { _, _ -> })
                 .setPositiveButton(R.string.save, { _, _ ->
-                    val newCar = OwnCar(regNoView.text.toString(), ownerView.text.toString()) //todo: improve
+                    val newCar = OwnCar(regNoView.text.toString(), ownerView.text.toString())
                     listener?.onDialogPositiveClick(newCar)})
         val dialog = builder.create()
         dialog.window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM)
         dialog.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
         return dialog
     }
+
+    private fun updatePositiveButton() {
+        val canSave = !TextUtils.isEmpty(regNoView.text) && !TextUtils.isEmpty(ownerView.text)
+        (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = canSave
+    }
+
+    inner class DialogTextWatcher(): TextWatcher {
+        override fun afterTextChanged(s: Editable?) = updatePositiveButton()
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+    }
+
 }
