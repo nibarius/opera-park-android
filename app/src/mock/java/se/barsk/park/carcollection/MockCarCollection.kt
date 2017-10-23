93d52aac49745c0ddfc64c973ddd9ffd0e7dafba
Niklas Barsk <nibarius@gmail.com>
2017-10-23 21:56:06 +0200

    Make number of cars configurable in the mock car collection
    Makes it more testable with unit tests.

--- app/src/mock/java/se/barsk/park/carcollection/MockCarCollection.kt
+++ app/src/mock/java/se/barsk/park/carcollection/MockCarCollection.kt
@@ -4,11 +4,11 @@ package se.barsk.park.datatypes
  * A mock implementation of the CarCollection with a hard coded list of cars to
  * get well defined data and stable data. It does not persist any changes to storage.
  */
-class MockCarCollection : CarCollection() {
+class MockCarCollection(var numCars: Int = 2) : CarCollection() {
     override fun readCarsFromStorage(): MutableList<OwnCar> {
         val car1 = OwnCar("ALP 110", "Margaretha")
         val car2 = OwnCar("MLB 803", "Margaretha")
-        return mutableListOf(car1, car2)
+        return listOf(car1, car2).subList(0, numCars).toMutableList()
     }
 
     override fun persistRemoval(ownCar: OwnCar) = Unit
