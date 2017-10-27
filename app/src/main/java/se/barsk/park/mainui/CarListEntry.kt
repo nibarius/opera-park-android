2c482cf4d32ebda895a1c4de9f26cf0d422c577d
Niklas Barsk <nibarius@gmail.com>
2017-10-27 21:58:38 +0200

    Fix oversized check mark when selecting cars
    The checkmark in the manage cars view uses padding while the avatars doesn't.
    So preserve the padding rather than removing it.

--- app/src/main/java/se/barsk/park/mainui/CarListEntry.kt
+++ app/src/main/java/se/barsk/park/mainui/CarListEntry.kt
@@ -2,6 +2,7 @@ package se.barsk.park.mainui
 
 import android.content.Context
 import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import android.view.View
 import se.barsk.park.R
@@ -19,12 +20,21 @@ interface CarListEntry {
     fun setAvatarColor(color: Int, context: Context, view: View) {
         val drawable = ContextCompat.getDrawable(context, R.drawable.circle_drawable)
         drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
-        view.background = drawable
-        view.setPadding(0, 0, 0, 0) // setting background resets padding, remove it again
+        setBackgroundPreservePadding(view, drawable)
     }
 
     fun getColorForCar(car: Car, context: Context): Int {
         val colors = context.resources.getIntArray(R.array.avatar_background_color)
         return colors[Math.abs(car.regNo.hashCode()) % colors.size]
+    }
+
+    private fun setBackgroundPreservePadding(view: View, background: Drawable) {
+        val pL = view.paddingLeft
+        val pT = view.paddingTop
+        val pR = view.paddingRight
+        val pB = view.paddingBottom
+
+        view.background = background
+        view.setPadding(pL, pT, pR, pB)
     }
 }
