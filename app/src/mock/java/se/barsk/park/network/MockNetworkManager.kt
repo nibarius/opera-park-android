f8454ef21114469efbf4682ea62794349949e38d
Niklas Barsk <nibarius@gmail.com>
2017-11-02 21:40:23 +0100

    Make it possible to simulate no connection with MockNetworkManager

--- app/src/mock/java/se/barsk/park/network/MockNetworkManager.kt
+++ app/src/mock/java/se/barsk/park/network/MockNetworkManager.kt
@@ -17,27 +17,36 @@ class MockNetworkManager(private val initialParkedCars: Int = BuildConfig.initia
     private var parkedCars: MutableList<ParkedCar> = mutableListOf()
     private val networkDelay: Long = 300
 
+    var hasConnection = true
+
     override fun readServerFromStorage() = "http://park.example.com"
 
     override fun checkStatus(context: Context, resultReadyListener: (Result) -> Unit) {
         updateState = UpdateState.UPDATE_IN_PROGRESS
         if (parkedCars.isEmpty()) {
             val initial = listOf(
-                        ParkedCar("DZU 241", "Henrik", "2017-10-01 08:05:15"),
-                        ParkedCar("CRF 461", "Erik", "2017-10-01 08:16:55"),
-                        ParkedCar("WNF 766", "Rikard", "2017-10-01 08:21:06"),
-                        ParkedCar("AGF 487", "Niklas", "2017-10-01 08:29:53"),
-                        ParkedCar("ALP 110", "Margaretha", "2017-10-29 08:38:14"),
-                        ParkedCar("MLB 942", "Per", "2017-10-01 09:01:33"))
+                    ParkedCar("DZU 241", "Henrik", "2017-10-01 08:05:15"),
+                    ParkedCar("CRF 461", "Erik", "2017-10-01 08:16:55"),
+                    ParkedCar("WNF 766", "Rikard", "2017-10-01 08:21:06"),
+                    ParkedCar("AGF 487", "Niklas", "2017-10-01 08:29:53"),
+                    ParkedCar("ALP 110", "Margaretha", "2017-10-29 08:38:14"),
+                    ParkedCar("MLB 942", "Per", "2017-10-01 09:01:33"))
             parkedCars = initial.subList(0, initialParkedCars).toMutableList()
         }
         Handler().postDelayed({ resultReady(resultReadyListener) }, networkDelay)
     }
 
     private fun resultReady(resultReadyListener: (Result) -> Unit) {
-        updateState = UpdateState.IDLE
-        state = State.HAVE_MADE_SUCCESSFUL_REQUEST
-        resultReadyListener(Result.Success(parkedCars))
+        if (hasConnection) {
+            updateState = UpdateState.IDLE
+            state = State.HAVE_MADE_SUCCESSFUL_REQUEST
+            resultReadyListener(Result.Success(parkedCars))
+        } else {
+            if (state == State.FIRST_RESPONSE_NOT_RECEIVED) {
+                state = State.ONLY_FAILED_REQUESTS
+            }
+            resultReadyListener(Result.Fail(null, ""))
+        }
     }
 
     override fun parkCar(context: Context, ownCar: OwnCar, resultReadyListener: (Result) -> Unit) {
@@ -49,6 +58,9 @@ class MockNetworkManager(private val initialParkedCars: Int = BuildConfig.initia
     }
 
     private fun addIfNotExist(ownCar: OwnCar, resultReadyListener: (Result) -> Unit) {
+        if (!hasConnection) {
+            return
+        }
         parkedCars
                 .filter { it.regNo == ownCar.regNo }
                 .forEach {
@@ -60,6 +72,9 @@ class MockNetworkManager(private val initialParkedCars: Int = BuildConfig.initia
     }
 
     private fun removeIfExists(ownCar: OwnCar, resultReadyListener: (Result) -> Unit) {
+        if (!hasConnection) {
+            return
+        }
         (parkedCars.size - 1 downTo 0)
                 .filter { parkedCars[it].regNo == ownCar.regNo }
                 .forEach { parkedCars.removeAt(it) }
