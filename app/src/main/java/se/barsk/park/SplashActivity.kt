59d56444eca34c635bc4cd19880a7a01d73a67a0
Niklas Barsk <nibarius@gmail.com>
2017-11-19 20:56:20 +0100

    Introduce a splash screen with a placeholder UI
    The splash is shown until the main UI is loaded.

---
+++ app/src/main/java/se/barsk/park/SplashActivity.kt
@@ -0,0 +1,21 @@
+package se.barsk.park
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import se.barsk.park.mainui.ParkActivity
+
+// Inspiration from:
+// https://www.bignerdranch.com/blog/splash-screens-the-right-way/
+// https://medium.com/@lucasurbas/placeholder-ui-launch-screen-d85c35552119
+/**
+ * Activity for the splash screen that just opens the ParkActivity
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, ParkActivity::class.java))
+        finish()
+    }
+}
