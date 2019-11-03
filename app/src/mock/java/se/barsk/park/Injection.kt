10c73d955349bc3d03c2949e991e12deedbab883
Niklas Barsk <nibarius@gmail.com>
2019-11-03 22:20:17 +0100

    Silence warning

--- app/src/mock/java/se/barsk/park/Injection.kt
+++ app/src/mock/java/se/barsk/park/Injection.kt
@@ -6,6 +6,9 @@ import se.barsk.park.datatypes.MockUser
 import se.barsk.park.network.MockNetworkManager
 import se.barsk.park.storage.MockStorageManager
 
+// The Injection object in the mock flavor might not need to use all parameters required
+// by the production flavor
+@Suppress("UNUSED_PARAMETER")
 object Injection {
     fun provideNetworkManager() = MockNetworkManager()
     fun provideCarCollection() = MockCarCollection()
